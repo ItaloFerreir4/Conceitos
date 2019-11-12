f840134de58760f026179018731f24e89b10e5c3
Italo Ferreira <italo.s.ferreira@hotmail.com>
2019-11-12 19:47:37 -0300

    asd

---
+++ Conceito/Conceito/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conceito
+{
+    class Item
+    {
+        public string NomeItem;
+        public double ValorItem;
+        public int QuantidadeItem;
+
+        public double AtualizaIventario()
+        {
+            return ValorItem * QuantidadeItem;
+        }
+        public void AdicionaItens(int quantidade)
+        {
+            QuantidadeItem += quantidade;
+        }
+        public void RemoveItens(int quantidade)
+        {
+            QuantidadeItem -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return NomeItem
+                + ", $"
+                + ValorItem.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + QuantidadeItem
+                + " unidades, Total: $"
+                + AtualizaIventario().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Conceito/Conceito/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Conceito
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            Item i = new Item();
+            bool continua = true;
+
+            while (continua)
+            {
+                Clear();
+                WriteLine("__________Entre com os dados do item__________");
+                Write("Nome do item: ");
+                i.NomeItem = ReadLine();
+                Write("Valor do item: ");
+                i.ValorItem = double.Parse(ReadLine(), CultureInfo.InstalledUICulture);
+                Write("Quantidade do inventario: ");
+                i.QuantidadeItem = int.Parse(ReadLine());
+                WriteLine("");
+
+                WriteLine("Dados do item: " + i);
+                WriteLine("");
+
+                Write("Digite o numero de itens a ser adicionado ao inventario: ");
+                int qtda = int.Parse(ReadLine());
+                i.AdicionaItens(qtda);
+                WriteLine("");
+
+                WriteLine("Dados atualizados: " + i);
+                WriteLine("");
+
+                Write("Digite o numero de itens a ser removido ao inventario: ");
+                qtda = int.Parse(ReadLine());
+                i.RemoveItens(qtda);
+                WriteLine("");
+
+                WriteLine("Dados atualizados: " + i);
+                WriteLine("");
+
+                WriteLine("Deseja continuar (sim ou nao): ");
+                string vai = ReadLine();
+
+                switch (vai.ToLower())
+                {
+                    case "sim":
+                        continua = true;
+                        break;
+                    case "nao":
+                        continua = false;
+                        break;
+                    default:
+                        {                          
+                            WriteLine("__________!!Favor digitar apenas sim ou nao (sem acentos)!!__________");
+                            WriteLine("Deseja continuar (sim ou nao): ");
+                            vai = ReadLine();
+                            break;
+                        }
+                }
+            }
+
+            WriteLine("-___________Volte Sempre___________-");
+            ReadLine();
+        }
+    }
+}
